fbfda4a33ea4eeb3a59a1899cb15962a241e9e33
davask <davask.42@gmail.com>
2016-09-22 03:15:10 +0200

    Update init process

--- tmp/dwl/openssl.sh
+++ tmp/dwl/openssl.sh
@@ -1,6 +1,7 @@
 for conf in `find /etc/apache2/sites-available -type f -name "*.conf"`; do
 
-  DWL_USER_DNS=`echo ${conf} | awk -F '[/]' '{print $5}' | sed "s|\.conf||g";
+  DWL_USER_DNS=`echo ${conf} | awk -F '[/]' '{print $5}' | sed "s|\.conf||g"`;
+  echo ${DWL_USER_DNS};
 
   if [ ! -d ${APACHE_SSL_DIR}/${DWL_USER_DNS} ]; then
       mkdir -p ${APACHE_SSL_DIR}/${DWL_USER_DNS};
@@ -15,27 +16,27 @@ for conf in `find /etc/apache2/sites-available -type f -name "*.conf"`; do
        -x509 -days 90 -out ${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.crt \
        -subj "/C=${DWL_SSLKEY_C}/ST=${DWL_SSLKEY_ST}/L=${DWL_SSLKEY_L}/O=${DWL_SSLKEY_O}/CN=${DWL_SSLKEY_CN}";
 
-    if [ "$((`echo ${DWL_USER_DNS} | sed 's/[^\.]//g' | wc -c` -1 ))" != 1 ]; then
+    if [ "$((`echo "${DWL_USER_DNS}" | sed 's/[^\.]//g' | wc -c` -1 ))" != 1 ]; then
 
-      DWL_USER_DNS_SERVERNAME=`echo ${DWL_USER_DNS} | awk -F '[\.]' '{print $(NF-1)"."$NF}'`;
+      DWL_USER_DNS_SERVERNAME=`echo "${DWL_USER_DNS}" | awk -F '[\.]' '{print $(NF-1)"."$NF}'`;
 
-      sed -i "s|# ServerName|ServerName `echo ${DWL_USER_DNS_SERVERNAME}`|g" \
+      sed -i "s|# ServerName|ServerName `echo "${DWL_USER_DNS_SERVERNAME}"`|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
 
-      sed -i "s|# ServerAlias|ServerAlias `echo ${DWL_USER_DNS}`|g" \
+      sed -i "s|# ServerAlias|ServerAlias `echo "${DWL_USER_DNS}"`|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
 
-      sed -i "s|# SSLCertificateFile|SSLCertificateFile `echo ${APACHE_SSL_DIR}/${DWL_USER_DNS_SERVERNAME}`/apache.crt|g" \
+      sed -i "s|# SSLCertificateFile|SSLCertificateFile `echo "${APACHE_SSL_DIR}/${DWL_USER_DNS_SERVERNAME}"`/apache.crt|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
 
-      sed -i "s|# SSLCertificateKeyFile|SSLCertificateKeyFile `echo ${APACHE_SSL_DIR}/${DWL_USER_DNS_SERVERNAME}`/apache.key|g" \
+      sed -i "s|# SSLCertificateKeyFile|SSLCertificateKeyFile `echo "${APACHE_SSL_DIR}/${DWL_USER_DNS_SERVERNAME}"`/apache.key|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
     else
-      sed -i "s|# ServerName|ServerName `echo ${DWL_USER_DNS}`|g" \
+      sed -i "s|# ServerName|ServerName `echo "${DWL_USER_DNS}"`|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
-      sed -i "s|# SSLCertificateFile|SSLCertificateFile `echo ${APACHE_SSL_DIR}/${DWL_USER_DNS}`/apache.crt|g" \
+      sed -i "s|# SSLCertificateFile|SSLCertificateFile `echo "${APACHE_SSL_DIR}/${DWL_USER_DNS}"`/apache.crt|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
-      sed -i "s|# SSLCertificateKeyFile|SSLCertificateKeyFile `echo ${APACHE_SSL_DIR}/${DWL_USER_DNS}`/apache.key|g" \
+      sed -i "s|# SSLCertificateKeyFile|SSLCertificateKeyFile `echo "${APACHE_SSL_DIR}/${DWL_USER_DNS}"`/apache.key|g" \
           /etc/apache2/sites-available/${DWL_USER_DNS}.conf;
     fi
   fi
