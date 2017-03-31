930e58813cb7dad8daa9d0c0c4369475d638c119
davask <contact@davaskweblimited.com>
2017-03-31 17:47:47 +0200

    quick fix for dns activation

--- build/dwl/openssl.sh
+++ build/dwl/openssl.sh
@@ -6,8 +6,8 @@ for conf in `find /etc/apache2/sites-available -type f -name "*.conf"`; do
 
     DWL_USER_DNS_DATA=`echo ${DWL_USER_DNS_CONF} | awk -F '[/]' '{print $5}' | sed "s|\.conf||g"`;
 
-    DWL_USER_DNS=`echo ${DWL_USER_DNS_DATA} | awk -F '[-]' '{print $2}'`;
-    DWL_USER_DNS_PORT=`echo ${DWL_USER_DNS_DATA} | awk -F '[-]' '{print $3}'`;
+    DWL_USER_DNS=`echo ${DWL_USER_DNS_DATA} | awk -F '[_]' '{print $2}'`;
+    DWL_USER_DNS_PORT=`echo ${DWL_USER_DNS_DATA} | awk -F '[_]' '{print $3}'`;
     DWL_USER_DNS_SERVERNAME=`echo "${DWL_USER_DNS}" | awk -F '[\.]' '{print $(NF-1)"."$NF}'`;
 
     echo "> configure openssl virtualhost for ${DWL_USER_DNS} with path ${DWL_USER_DNS_CONF}";
