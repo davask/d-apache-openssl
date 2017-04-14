261ebdf779b8e0ca48208602bf2e2a952a95e747
davask <contact@davaskweblimited.com>
2017-04-14 14:46:05 +0200

    Automatic update 2.4-u14.04

--- script/docker-compose.sh
+++ script/docker-compose.sh
@@ -21,20 +21,20 @@ echo "d-apache:
     DWL_LOCAL_LANG: en_US:en
     DWL_USER_NAME: username
     DWL_SSH_ACCESS: 'true'
+    DWL_SHIELD_HTTP: 'false'
     DWL_SSLKEY_C: EU
     DWL_SSLKEY_ST: France
     DWL_SSLKEY_L: Vannes
     DWL_SSLKEY_O: davask web limited - docker container
     DWL_SSLKEY_CN: davaskweblimited.com
-  log_driver: syslog
-  labels:
-    io.rancher.scheduler.affinity:host_label: dwl=dwlComPrivate
   image: davask/d-apache-openssl:${branch}
-  hostname: private.davaskweblimited.com
+  hostname: localhost
+  net: bridge
   volumes:
-  - ${buildDir}/home/username/files:/home/username/files
-  - ${buildDir}/home/username/http/app/sites-available:/etc/apache2/sites-available
   - ${buildDir}/etc/apache2/ssl:/etc/apache2/ssl
+  - ${rootDir}/volumes/proxy/log/localhost/apache2:/var/log/apache2
+  - ${rootDir}/volumes/home/username/http/app/sites-available:/etc/apache2/sites-available
+  - ${rootDir}/volumes/home/username/files:/home/username/files
   working_dir: /var/www/html
 " > ${rootDir}/docker-compose.yml
 
