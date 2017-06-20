e32267ec6016dfafd1c1a202435adcc26c86aece
davask <contact@davaskweblimited.com>
2017-06-20 08:31:22 +0200

    Automatic update 2.4-d8.8

--- build/dwl/virtualhost-ssl.sh
+++ build/dwl/virtualhost-ssl.sh
@@ -11,19 +11,22 @@ for conf in `sudo find /etc/apache2/sites-available -type f -name "*.conf"`; do
     DWL_USER_DNS_PORT_CONTAINER=`echo ${DWL_USER_DNS_DATA} | awk -F '[_]' '{print $1}'`;
     DWL_USER_DNS_SERVERNAME=`echo "${DWL_USER_DNS}" | awk -F '[\.]' '{print $(NF-1)"."$NF}'`;
 
-    if [ "$DWL_USER_DNS_PORT" == "443" ] \
-    && [ ! -f "/etc/letsencrypt/live/${DWL_USER_DNS}/cert.pem" ] \
-    && [ ! -f "/etc/letsencrypt/live/${DWL_USER_DNS}/privkey.pem" ] \
-    && [ ! -f "/etc/letsencrypt/live/${DWL_USER_DNS}/chain.pem" ] \
-    && [ -f "${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.key" ] \
-    && [ -f "${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.crt" ]; then
-
+    if [ "$DWL_USER_DNS_PORT" == "443" ]; then
         echo "> configure virtualhost-ssl for ${DWL_USER_DNS} with path ${DWL_USER_DNS_CONF}";
+        if [ ! -f "/etc/letsencrypt/live/${DWL_USER_DNS}/cert.pem" ] \
+        && [ -f "${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.key" ]; then
 
-        echo "Update SSL Certificat for domain";
+            echo "Update SSL Certificat public key for domain";
+            sudo sed -i "s|# SSLCertificateFile|SSLCertificateFile ${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.crt|g" ${DWL_USER_DNS_CONF};
 
-        sudo sed -i "s|# SSLCertificateFile|SSLCertificateFile ${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.crt|g" ${DWL_USER_DNS_CONF};
-        sudo sed -i "s|# SSLCertificateKeyFile|SSLCertificateKeyFile ${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.key|g" ${DWL_USER_DNS_CONF};
+        fi
+        if [ ! -f "/etc/letsencrypt/live/${DWL_USER_DNS}/privkey.pem" ] \
+        && [ -f "${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.crt" ]; then
+
+            echo "Update SSL Certificat private key for domain";
+            sudo sed -i "s|# SSLCertificateKeyFile|SSLCertificateKeyFile ${APACHE_SSL_DIR}/${DWL_USER_DNS}/apache.key|g" ${DWL_USER_DNS_CONF};
+
+        fi
     fi
 
 done;
