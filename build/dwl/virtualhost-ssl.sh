a2525cc948478d84d08a99cb384880d0fd609107
davask <contact@davaskweblimited.com>
2017-07-06 23:54:21 +0200

    Automatic update 2.4-u14.04

--- build/dwl/virtualhost-ssl.sh
+++ build/dwl/virtualhost-ssl.sh
@@ -2,14 +2,7 @@
 
 for conf in `sudo find /etc/apache2/sites-available -type f -name "*.conf"`; do
 
-    DWL_USER_DNS_CONF=${conf};
-
-    DWL_USER_DNS_DATA=`echo ${DWL_USER_DNS_CONF} | awk -F '[/]' '{print $5}' | sed "s|\.conf||g"`;
-
-    DWL_USER_DNS=`echo ${DWL_USER_DNS_DATA} | awk -F '[_]' '{print $2}'`;
-    DWL_USER_DNS_PORT=`echo ${DWL_USER_DNS_DATA} | awk -F '[_]' '{print $3}'`;
-    DWL_USER_DNS_PORT_CONTAINER=`echo ${DWL_USER_DNS_DATA} | awk -F '[_]' '{print $1}'`;
-    DWL_USER_DNS_SERVERNAME=`echo "${DWL_USER_DNS}" | awk -F '[\.]' '{print $(NF-1)"."$NF}'`;
+    . ./vhost-env.conf;
 
     if [ "$DWL_USER_DNS_PORT" == "443" ]; then
         echo "> configure virtualhost-ssl for ${DWL_USER_DNS} with path ${DWL_USER_DNS_CONF}";
